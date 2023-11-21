f4589062dfa1d14e637f933c23ed949b61621a06
timoxoszt <20520810@gm.uit.edu.vn>
2023-11-22 00:33:50 +0700

    flag3 leak credential webserver

--- CoolScripts/flag3.sh
+++ CoolScripts/flag3.sh
@@ -1,1 +1,38 @@
-echo YW5nZWxhIDogV2VicGFzczEyMyB8IFRyeWVsbCA6IFdpbkNsaWVudDEyMw== > /var/www/html/public/c4nn07_BRut3f0rc3.txt
+#!/bin/bash
+
+# Target file path
+TARGET_FILE="/var/www/html/public/deploy.sh"
+
+# Writing the script to the target file
+cat << 'EOF' > "$TARGET_FILE"
+#!/bin/bash
+
+# Variables
+HOST="172.16.0.10"
+USERNAME="angela"
+PASSWORD="Webpass123"
+LOCAL_PATH="*"  # All files in the current directory
+REMOTE_PATH="/var/www/html/public/"
+
+# Install sshpass if not already installed (Ubuntu/Debian)
+if ! command -v sshpass &> /dev/null; then
+    echo "sshpass could not be found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y sshpass
+fi
+
+# Using sshpass to automate password entry for scp
+sshpass -p "$PASSWORD" scp $LOCAL_PATH "${USERNAME}@${HOST}:${REMOTE_PATH}"
+
+# Check if the copy was successful
+if [ $? -eq 0 ]; then
+    echo "File transfer successful."
+else
+    echo "File transfer failed."
+fi
+EOF
+
+# Giving execute permissions to the script
+chmod +x "$TARGET_FILE"
+
+echo "Script written to $TARGET_FILE and made executable."
